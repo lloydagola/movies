b54a54af9798f21186a83b06263a7b778ce1a792
lloydagola <lloydagola@users.noreply.github.com>
2024-07-20 21:40:33 +0300

    refactored Movies into separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import React from "react";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import Chip from "@mui/material/Chip";
-import { TMovie, TMovieCardProps } from "./types/types";
 import movieData from "./_mocks/movies";
 
 import "./App.css";
 import Box from "@mui/material/Box";
-import MovieCard from "./components/MovieCard";
+import Movies from "./components/Movies";
 
 function App() {
   return (
@@ -18,9 +10,7 @@ function App() {
       <header className="App-header"></header>
       <body>
         <Box>
-          {movieData.map((movie: TMovie, index: number) => (
-            <MovieCard key={index} movie={movie} />
-          ))}
+          <Movies movies={movieData} />
         </Box>
       </body>
     </div>
---
+++ src/components/Movies.tsx
@@ -0,0 +1,22 @@
+import Grid from "@mui/material/Grid";
+import MovieCard from "./MovieCard";
+
+type TMovieListProps = { movies: any[] };
+
+export default function Movies({ movies }: TMovieListProps): JSX.Element {
+  return (
+    <Grid
+      container
+      gap={2}
+      m="auto"
+      sx={{
+        gridTemplateColumns: "auto-fill, minMax(280px, 1fr)",
+        margin: "auto",
+      }}
+    >
+      {movies.map((movie, index) => (
+        <MovieCard key={index} movie={movie} />
+      ))}
+    </Grid>
+  );
+}
