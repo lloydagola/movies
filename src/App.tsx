d9244f761eb6b2449343d4f4b2c76f3c3d2c1a3d
lloydagola <lloydagola@users.noreply.github.com>
2024-07-20 21:34:20 +0300

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,7 @@ import movieData from "./_mocks/movies";
 
 import "./App.css";
 import Box from "@mui/material/Box";
-
-function MovieCard({ movie }: TMovieCardProps): JSX.Element {
-  return (
-    <Grid item>
-      <Card sx={{ width: "280px" }}>
-        <CardMedia
-          component="img"
-          alt={movie.title}
-          sx={{ width: "280px" }}
-          image={movie.posterUrl}
-        />
-        <CardContent>
-          <Typography variant="h6">{movie.title}</Typography>
-          <Typography variant="body2">{movie.snippet}</Typography>
-          <div>
-            {movie.genres.map((genre: string) => (
-              <Chip key={genre} label={genre} />
-            ))}
-          </div>
-        </CardContent>
-      </Card>
-    </Grid>
-  );
-}
+import MovieCard from "./components/MovieCard";
 
 function App() {
   return (
