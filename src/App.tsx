1daa63d34837d852622b5d1d4cb67422642cb769
lloydagola <lloydagola@users.noreply.github.com>
2024-07-20 22:35:10 +0300

    implemented basic pagination

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,25 @@ import SearchBar from "./components/SearchBar";
 import filterMovies from "./utils/filterMovies";
 import movies from "./_mocks/movies";
 import "./App.css";
+import { Pagination } from "@mui/material";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [page, setPage] = useState(1);
   const dataFiltered = filterMovies(searchQuery, movies);
+  const itemsPerPage = 2;
+  const pageCount = Math.ceil(dataFiltered.length / itemsPerPage);
+
+  const startIndex = (page - 1) * itemsPerPage;
+  const visibleMovies = dataFiltered.slice(
+    startIndex,
+    startIndex + itemsPerPage
+  );
+
+  function handlePageChange(_: unknown, page: number) {
+    setPage(page);
+  }
+
   return (
     <Box component="main" className="App">
       <Box component="header" className="App-header">
@@ -18,7 +33,14 @@ function App() {
       </Box>
       <Box component="body">
         <Box component="section">
-          <Movies movies={dataFiltered} />
+          <Movies movies={visibleMovies} />
+        </Box>
+        <Box component="footer">
+          <Pagination
+            count={pageCount}
+            page={page}
+            onChange={handlePageChange}
+          />
         </Box>
       </Box>
     </Box>
